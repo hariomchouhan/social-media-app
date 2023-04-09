570421fcf5b1a1d2c2d59c9fa27c0f9c84d524eb
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-04-09 17:01:25 +0530

    complete profile section and move to the post section

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from '../img/img1.png';
+import img2 from '../img/img2.png';
+import img3 from '../img/img3.png';
+import img4 from '../img/img4.jpg';
+
+export const Followers =[
+    { name: 'Hariom Chouhan', username: 'HariomChouhan', img: img1 },
+    { name: 'Niranjan Chouhan', username: 'NiranjanChouhan', img: img2 },
+    { name: 'Yashraj Chouhan', username: 'YashrajChouhan', img: img3 },
+    { name: 'Sachin Chouhan', username: 'SachinChouhan', img: img4 },
+];
---
+++ src/components/PostSide/PostSide.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function PostSide() {
+  return (
+    <div>
+      PostSide
+    </div>
+  )
+}
