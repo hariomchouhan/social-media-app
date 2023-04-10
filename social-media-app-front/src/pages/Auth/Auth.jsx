c86f42b2135b93229c441894e4cc04b63f2e1604
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-04-10 14:38:10 +0530

    complete frontend part and move to the server side part

---
+++ social-media-app-front/src/pages/Auth/Auth.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import "./Auth.css";
+import Logo from "../../img/logo.png";
+
+export default function Auth() {
+  return (
+    <div className="Auth">
+      <div className="a-left">
+        <img src={Logo} alt="" />
+        <div className="Webname">
+          <h1>HCC Media</h1>
+          <h6>Explore the ideas throughout the world.</h6>
+        </div>
+      </div>
+      <LogIn />
+    </div>
+  );
+}
+
+function LogIn() {
+    return (
+      <div className="a-right">
+        <form className="infoForm authForm">
+          <h3>Log In</h3>
+  
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+              className="infoInput"
+              name="username"
+            />
+          </div>
+  
+          <div>
+            <input
+              type="password"
+              className="infoInput"
+              placeholder="Password"
+              name="password"
+            />
+          </div>
+  
+          <div>
+              <span style={{ fontSize: "12px" }}>
+                Don't have an account Sign up
+              </span>
+            <button className="button infoButton">Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+
+function Signup() {
+  return (
+    <div className="a-right">
+      <form className="infoForm authForm">
+        <h3>Sign up</h3>
+
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            className="infoInput"
+            name="firstname"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="infoInput"
+            name="lastname"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            placeholder="Username"
+            name="username"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            placeholder="Password"
+            name="password"
+          />
+          <input
+            type="text"
+            className="infoInput"
+            placeholder="Confirm Password"
+            name="confirmpass"
+          />
+        </div>
+
+        <div>
+          <span style={{fontSize: '12px'}}>Already have an account. Login!</span>
+        </div>
+
+        <button className="button infoButton">Signup</button>
+      </form>
+    </div>
+  );
+}
